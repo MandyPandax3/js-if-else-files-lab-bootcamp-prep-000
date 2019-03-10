0679b9630eac0228c5e5b0ab371157fca12164b6
Mandy Strickland <mandy.strickland92@gmail.com>
2019-03-10 04:05:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-var name = "Susan"
+var name = "Sandy"
 var height = 70
 
 if (name === "Susan") {
   console.log("The name is correct.")
+} else{
+  console.log("Expected the name to be Susan.")
 }
 
 // Don't worry about this
